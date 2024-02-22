b7b7c9dfd5dfb7e49dec193b02855e103144f67e
JakobKvejborgUcn <10513107@ucn.dk>
2024-02-22 09:08:46 +0100

    First commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿// To open a new VS code project in C#:
+// 0. install dotnet 8 (google)
+// 1. Open CMD
+// 2. cd path\to\desired\directory
+// 3. mkdir MyCSharpProjectName
+// 4. cd MyCSharpProjectName
+// 5. dotnet new console
+// 6. code .  (this starts vs code)
+
+// How to clone repository from GitHub:
+// 1. Create a folder
+// 2. In terminal: (ctrl + æ) cd path/to/your/directory
+// 3. In terminal: git clone ""insert github clone repository URL here""
+// 4. In terminal: code repository
+
+
+
+// run the code:
+// open terminal: ctrl + shift + æ
+// dotnet run 
+
+
+Console.WriteLine("Hello, World!");
+
+
